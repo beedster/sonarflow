5018a7ea772cf45accc69885fca44b55535f9415
beedster <me@gitster>
2012-06-28 09:23:45 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sonarflow::Application.config.secret_token = '4bbb09c83036819b0b73d8aa4ec38fda21976981f5bacac87d22d8bc3fc40d425947fbd93e8ff132482935fe5544dc079637602bdbe6bced092f6da30d1dc887'
